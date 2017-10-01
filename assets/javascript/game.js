7c81e130daed4cb78234e2c9b3107ad5dee8bb86
Andrew Ferrer <AndBFer@Andrews-MacBook-Pro.local>
2017-10-01 12:10:51 -0600

    note-taking, I am leaving this coffee shop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,10 @@ var skiBucket = ["tomahawk, full-send, camber, rocker, avalanche, powder, pow-da
 var userGuess = document.getElementById("guessed");
 
 //this is rewriting the entire element, not accruing letters
+//holding command+R after a refresh removes the "Meta", there needs to be 
+//a "span" element that is rewritten inside the "guessed" div.
+//use the div for styling purposed and the "span" for holding text.
+//make sure it begins to accrue letters
 document.onkeyup = function(event) {
         userGuess.textContent = event.key;
       };
