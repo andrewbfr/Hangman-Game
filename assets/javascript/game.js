258cdec7dba5cae1ad118cdf53771b2b8c5f94b3
Andrew Ferrer <AndBFer@Andrews-MacBook-Pro.local>
2017-10-01 11:42:49 -0600

    Im not sure if Im leaving this coffee shop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
-var rockBucket = [""]
+var rockBucket = ["whipper, cam-a-lot, tri-cam, rope, sticky, figure-eight, granite, sloper, sandstone"]
 
-var skiBucket = []
+var skiBucket = ["tomahawk, full-send, camber, rocker, avalanche, powder, pow-day"]
+
+
