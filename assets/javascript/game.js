545db4143fe35879a0c8e40e4ef8690b981a8795
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2017-10-06 22:33:41 -0600

    working with Benjamin

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,55 @@
 
 
 $(document).ready(function(){
-	// try this as a dual-page game later
-var rockBucket = ["whipper, cam-a-lot, tri-cam, rope, sticky, figure-eight, granite, sloper, sandstone"]
 
 // use this for the game
-var skiBucket = ["tomahawk, full-send, camber, rocker, avalanche, powder, pow-day, whipper, cam-a-lot, tri-cam, rope, sticky, figure-eight, granite, sloper, sandstone"]
 
 //nice start
-	console.log("a");
 
 	$(document).keyup(function() {
-	function generateWord() {
-
-	console.log("b");
-   //	return skiBucket[Math.floor(Math.random() * skiBucket.length)];
-   	var rand = skiBucket[Math.floor(Math.random() * skiBucket.length)];
-	$("#correct").text(rand);
-
-	};
-
-	console.log("c");
+		var newWord = generateWord();
+		$("#correct").text(newWord);
 
 	});
 
 
-	// var rand = skiBucket[Math.floor(Math.random() * skiBucket.length)];
-	// $("#correct").text(rand);
-
-
-
-
-
-//this is rewriting the entire element, not accruing letters
-//holding command+R after a refresh removes the "Meta", there needs to be 
-//a "span" element that is rewritten inside the "guessed" div.
-//use the div for styling purposed and the "span" for holding text.
-//make sure it begins to accrue letters
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
  });//closing document.ready
+
+var skiBucket = ["tomahawk", "full-send", "camber", "rocker", "avalanche", "powder", "pow-day", "whipper", "cam-a-lot", "tri-cam", "rope", "sticky", "figure-eight", "granite", "sloper", "sandstone"];
+
+
+function generateWord() {
+
+  	var skiNumber = Math.floor(Math.random() * skiBucket.length);
+   	var rand = skiBucket[skiNumber];
+   	return rand;
+
+	}
+
+	//developing functions for each step in the process
+function showCorrectGuesses(character){
+
+}
+
+function saveIncorrectGuesses(character){
+
+}
+
+function checkIfCharPresent(word, character){
+	if (word.includes(character)){ 
+		return true;
+
+	}
+	else {
+		return false;
+	}
+}
+
+
+
+
+
 
 
 
